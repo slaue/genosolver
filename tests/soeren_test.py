d7b900006083a4149d927a3c3319bf5db464f3df
Tomo <tprusina@mathos.hr>
2023-05-24 14:17:23 +0200

    Moved failing tests to soeren_test

--- tests/soeren_test.py
+++ tests/soeren_test.py
@@ -10,6 +10,94 @@ import numpy as np
 from genosolver import minimize
 from scipy.optimize import minimize as s_min
 
+def test_bound_constrained1():
+    def fg1(x):
+        Q = np.array([[1., 0, 1],
+                      [0, 2, 0],
+                      [1, 0, 3]])
+        c = np.array([2., 3, 4])
+        t = Q.dot(x)
+        f = 0.5 * x.dot(t) + c.dot(x)
+        g = t + c
+        return f, g
+
+    options = {'verbose' : 100,
+               'eps_pg' : 1E-6,
+               'm' : 10,
+               'max_iter' : 1000,
+               'grad_test' : False,
+               'ls' : 0}
+
+    x0 = np.zeros(3)
+    res = minimize(fg1, x0, options=options, np=np)
+    assert_allclose(res.x, np.array([-1, -1.5, -1]), atol=1E-4)
+
+    x0 = np.zeros(3)
+    lb = np.full(3, -1.)
+    ub = np.full(3, 5)
+    res = minimize(fg1, x0, lb=lb, ub=ub, np=np)
+    assert_allclose(res.x, np.array([-1, -1, -1]), atol=1E-4)
+
+    lb = np.full(3, -1.2)
+    ub = np.full(3, 5)
+    res = minimize(fg1, x0, lb=lb, ub=ub, np=np)
+    assert_allclose(res.x, np.array([-1, -1.2, -1]), atol=1E-4)
+
+    lb = np.full(3, -2)
+    ub = np.full(3, 5)
+    res = minimize(fg1, x0, lb=lb, ub=ub, np=np)
+    assert_allclose(res.x, np.array([-1, -1.5, -1]), atol=1E-4)
+
+
+def test_constrained0():
+    def fg(x):
+        n = len(x)
+        c = np.arange(n)
+        f = x.dot(x) + c.dot(x)
+        g = 2*x + c
+        return f, g
+
+    def constraint_f(x):
+        # sum(x) - 1 == 0
+        f = np.sum(x) - 1
+        return f
+
+    def constraint_jac_prod(x, y):
+        # sum(x) - 1 == 0
+        # return the product of the Jacobian and y
+        g = np.ones_like(x)
+        jp = y * g
+        return jp
+
+    constraints = ({'type' : 'eq',
+                    'fun' : constraint_f,
+                    'jacprod' : constraint_jac_prod})
+
+    options = {'eps_pg' : 1E-4,
+               'constraint_tol' : 1E-4,
+               'max_iter' : 3000,
+               'm' : 10,
+               'ls' : 0,
+               'verbose' : 10  # Set it to 0 to fully mute it.
+              }
+    n = 4
+    x0 = np.zeros(n)
+    res = minimize(fg, x0, constraints=constraints, options=options, np=np)
+    assert_allclose(res.x, np.array([1., 0.5, 0, -0.5]), atol=1E-4)
+
+    lb = np.full(n, 0)
+    ub = np.full(n, 1)
+    res = minimize(fg, x0, lb=lb, ub=ub, constraints=constraints, options=options, np=np)
+    assert_allclose(res.x, np.array([0.75, 0.25, 0, 0]), atol=1E-4)
+
+    lb = np.full(n, -0.2)
+    res = minimize(fg, x0, lb=lb, constraints=constraints, options=options, np=np)
+    assert_allclose(res.x, np.array([0.9, 0.4, -0.1, -0.2]), atol=1E-4)
+
+    ub = np.full(n, 0.5)
+    res = minimize(fg, x0, ub=ub, constraints=constraints, options=options, np=np)
+    assert_allclose(res.x, np.array([0.5, 0.5, 0.25, -0.25]), atol=1E-4)
+
 
 def test_brownbs():
     x0 = np.ones(2)
