9346643ad19a374ed7209e93771efbcb1f353498
Tomo <tprusina@mathos.hr>
2024-06-20 13:15:15 +0200

    Made number of iterations smaller

--- genosolver/bayesopt.py
+++ genosolver/bayesopt.py
@@ -625,6 +625,7 @@ def line_search_wolfe6(fg: Callable[[np.ndarray],tuple[float,np.ndarray]],
     gvals = [g_low, g]
     xvals = [lo, hi]
 
+    _i = -1
     if f_low >= finit:
         for _i in range(20):
             if f < finit:
@@ -663,7 +664,7 @@ def line_search_wolfe6(fg: Callable[[np.ndarray],tuple[float,np.ndarray]],
     
     default_ker = np.array([1e0, 1e-10])
     default_mu = np.linalg.lstsq(np.vander(gp.x, 4), gp.y, rcond=None)[0]#np.zeros_like(gp.mu.parameters)
-    for _i in range(20):
+    for _j in range(_i+1, 20):
         try:
             gp.ker.parameters = default_ker.copy()
             gp.mu.parameters = default_mu.copy()
